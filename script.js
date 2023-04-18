2ce3a6d36f7b47ffb547963bde47b80e106634ba
unknown <119958500@qq.com>
2023-04-18 09:15:23 +0800

    f

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 // Load CSV file
 
-d3.csv("./baise.csv").then(function(data) {
+d3.csv("baise.csv").then(function(data) {
 
   // Convert numerical values to numbers
   data.forEach(function(d) {
-    console.log(d)
-    d.rating = +d.rating;
+    //console.log(d)
+    d.time = new Date(d.time)
   });
+
+  // 筛选出符合日期范围的对象
+  const startDate = new Date("2021-08-01");
+  const endDate = new Date("2021-08-02");
+  const lastData = new Date("2021-8-31");
+
+  // 过滤出这几天的数据
+  const filteredList = data.filter(obj => {
+    return (obj.time >= startDate && obj.time <= endDate) || obj.time >= lastData;
+  });
+
+  // 排序
+  const sortedList = filteredList.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateA - dateB;
+  });
+  
+  console.log(sortedList);
+  $('#example').DataTable( {
+		data: sortedList,
+		columns: [
+			{ "data": "id" },
+			{ "data": "name" },
+			{ "data": "city" },
+			{ "data": "ratings" },
+			{ "data": "commet" },
+			{ "data": "time" }
+		]
+	} );
 
   // Define chart dimensions
   var margin = {top: 20, right: 20, bottom: 30, left: 150},
